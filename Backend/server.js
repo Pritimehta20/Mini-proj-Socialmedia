5d8b479be8281e67ba7ab51359ac3bd854992268
Pritimehta20 <mehta.priti.206@gmail.com>
2026-03-26 14:21:59 +0530

    fix  script

--- Backend/server.js
+++ Backend/server.js
@@ -15,10 +15,7 @@ app.use(cors({
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
-app.options("*", cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
+
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
